d429bfe3d55327e41d1ac1f8555f4bbf9c73433d
supersweet-dev <jim@supersweet.dev>
2022-06-02 17:46:20 -0500

    UNDER CONSTRUCTION

--- root/gatsby-config.js
+++ root/gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `SuperSweet`,
+    description: `Jim Alvarez's personal web site.`,
+    author: `@supersweet-dev`,
+    siteUrl: `https://supersweet.dev/`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -28,7 +28,7 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/ss-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- root/src/components/header.js
+++ root/src/components/header.js
@@ -5,7 +5,7 @@ import { Link } from "gatsby"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `#f7d521`,
       marginBottom: `1.45rem`,
     }}
   >
