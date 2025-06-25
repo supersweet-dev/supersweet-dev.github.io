2f04ed28ebee1c702667c94957b28e8b55ebdb79
supersweet-dev <jim@supersweet.dev>
2025-06-25 14:03:08 -0600

    Added vercel environment for local development. Finished /reviews api

--- src/pages/api/reviews.ts
+++ src/pages/api/reviews.ts
@@ -5,7 +5,35 @@ const LETTERBOXD_RSS_URL = 'https://letterboxd.com/pentagrami/rss';
 const TMDB_API_KEY = import.meta.env.TMDB_API_KEY;
 const TMDB_IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w342';
 
+type Review =
+	| {
+			error: false;
+			title: string;
+			year: string;
+			watchedAt: string;
+			score: string;
+			poster: string | null;
+			link: string;
+	  }
+	| {
+			error: true;
+			status?: number;
+	  };
+
+type RSSItem = {
+	'tmdb:movieId': string;
+	'letterboxd:filmTitle': string;
+	'letterboxd:filmYear': string;
+	'letterboxd:watchedDate'?: string;
+	'letterboxd:memberRating': string;
+	link: string;
+	pubDate: string;
+};
+
 export const GET: APIRoute = async () => {
+	if (!TMDB_API_KEY) {
+		return new Response('TMDB API key not configured', { status: 500 });
+	}
 	try {
 		const res = await fetch(LETTERBOXD_RSS_URL);
 		const xml = await res.text();
@@ -13,11 +41,10 @@ export const GET: APIRoute = async () => {
 		const parsed = await parseStringPromise(xml, { explicitArray: false });
 		const items = parsed.rss.channel.item;
 
-		// Limit to most recent 10
 		const recent = Array.isArray(items) ? items.slice(0, 10) : [items];
 
-		const reviews = await Promise.all(
-			recent.map(async (item: any) => {
+		const reviews = (await Promise.all(
+			recent.map(async (item: RSSItem) => {
 				const tmdbId = item['tmdb:movieId'];
 				const movieDetailsUrl = `https://api.themoviedb.org/3/movie/${tmdbId}?language=en-US`;
 				const details = await fetch(movieDetailsUrl, {
@@ -33,32 +60,28 @@ export const GET: APIRoute = async () => {
 						details.status
 					);
 					return {
-						data: item,
-						title: item.title,
-						year: item.pubDate,
-						poster: null,
+						status: details.status,
 						error: true,
-					};
+					} as Review;
 				}
 				const posterPath = (await details.json())['poster_path'];
 				const posterUrl = `${TMDB_IMAGE_BASE_URL}${posterPath}`;
 				return {
-					data: item,
+					error: false,
 					title: item['letterboxd:filmTitle'],
 					year: item['letterboxd:filmYear'],
 					watchedAt: item['letterboxd:watchedDate'] ?? item.pubDate,
 					score: item['letterboxd:memberRating'],
 					poster: posterPath ? posterUrl : null,
 					link: item.link,
-				};
+				} as Review;
 			})
-		);
+		)) as Review[];
 
 		return new Response(JSON.stringify(reviews), {
 			headers: { 'Content-Type': 'application/json' },
 		});
 	} catch (err) {
-		console.error('Error fetching or parsing RSS:', err);
 		return new Response('Failed to fetch RSS', { status: 500 });
 	}
 };
