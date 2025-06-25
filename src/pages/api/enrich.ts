a225a4b640fce434c4914d6bdd399a5933e8ec37
supersweet-dev <jim@supersweet.dev>
2025-06-25 00:50:03 -0600

    Testing updated fetch method.

--- src/pages/api/enrich.ts
+++ src/pages/api/enrich.ts
@@ -10,20 +10,43 @@ export const GET: APIRoute = ({ params, request }: any) => {
 
 export const POST: APIRoute = async ({ request }: any) => {
 	const TMDB_API_KEY = import.meta.env.TMDB_API_KEY;
-	console.log('TMDB_API_KEY:', TMDB_API_KEY);
+	if (!TMDB_API_KEY) {
+		console.error('TMDB_BEARER not defined in env vars');
+		return new Response('Server misconfiguration.', { status: 500 });
+	}
+
 	const reviews = await request.json();
 
 	const enriched = await Promise.all(
-		reviews.map(async (r: any) => {
-			const res = await fetch(
-				`https://api.themoviedb.org/3/search/movie?api_key=${TMDB_API_KEY}&query=${encodeURIComponent(
-					r.title
-				)}&year=${r.year}`
-			);
+		reviews.map(async (r: { title: string; year: string }) => {
+			const url = `https://api.themoviedb.org/3/search/movie?query=${encodeURIComponent(
+				r.title
+			)}&include_adult=true&language=en-US&primary_release_year=${
+				r.year
+			}&page=1`;
+
+			const res = await fetch(url, {
+				method: 'GET',
+				headers: {
+					accept: 'application/json',
+					Authorization: `Bearer ${TMDB_API_KEY}`,
+				},
+			});
+
+			if (!res.ok) {
+				console.error(`TMDb request failed for ${r.title}`, res.status);
+				return { ...r, poster: null, error: true };
+			}
+
 			const json = await res.json();
+			const posterPath = json?.results?.[0]?.poster_path || null;
+			const posterUrl = posterPath
+				? `https://image.tmdb.org/t/p/w342${posterPath}`
+				: null;
+
 			return {
 				...r,
-				tmdb: json,
+				poster: posterUrl,
 			};
 		})
 	);
