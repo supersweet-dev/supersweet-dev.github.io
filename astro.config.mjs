d187f55a3165c77e0bf39d5b65afe5a95be8ab3f
supersweet-dev <jim@supersweet.dev>
2025-06-25 00:19:34 -0600

    Adding Vercel astro config.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import vercelServerless from '@astrojs/vercel/serverless';
 import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [icon()],
 	site: 'https://supersweet.dev',
+	output: 'server',
+	adapter: vercelServerless({}),
 });
